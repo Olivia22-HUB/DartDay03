1b76486ddbd5b50465c7cda9834f8fda92e70354
Olivia22-HUB <oliviaurielle@gmail.com>
2025-01-30 23:19:29 +0100

    Add files via upload

--- exo1.dart
+++ exo1.dart
@@ -1,1 +1,40 @@
-class Amulette
+class Amulette {
+  //Propriété
+  String nom;
+  int puissance;
+  String materiaux;
+  //méthodes
+
+  //constructeur
+  Amulette(this.nom, this.puissance, this.materiaux);
+  @override
+  String toString() {
+    return ("L'amulette ${nom} fait à partir de ${materiaux} a une puissance de ${puissance}");
+  }
+}
+
+Amulette amuletteFeu = Amulette("Flamme Eternelle", 50, "Obsidienne");
+Amulette amuletteGlace = Amulette("Coeur Gelé", 40, "Glace");
+Amulette amuletteOmbre = Amulette("Voile Nocturne", 60, "Onyx");
+List<Amulette> amulettes = [amuletteFeu, amuletteGlace, amuletteOmbre];
+trouverAmulette(String nomRecherche) {
+  for (Amulette amulette in amulettes) {
+    if (amulette.nom == nomRecherche) {
+      return amulette;
+    }
+  }
+  return null;
+}
+
+void main() {
+  print(amuletteFeu);
+  print(amuletteGlace);
+  print(amuletteOmbre);
+  
+  var reponse = trouverAmulette("Nocturne Gelé");
+  if (reponse == null) {
+    print("Cette amulette n'existe pas encore dans l'atelier");
+  } else {
+    print("Disponible dans l'atelier");
+  }
+}
